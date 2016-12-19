44be9ecb0efd58f0c025ff9da05e1523eb1f83dd
Gao Xinyuan <294635134@qq.com>
2016-12-19 23:32:24 +0800

    create project

---
+++ XYMusicPlayer/XYMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYMusicPlayer
+//
+//  Created by 高昕源 on 2016/12/19.
+//  Copyright © 2016年 gxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
